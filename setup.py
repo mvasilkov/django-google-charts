948e96907b9c54e7eb50828a5592311aa5c5c970
Mark Vasilkov <mvasilkov@gmail.com>
2011-11-16 12:27:06 +0200

    Properly include templates

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name = 'django-google-charts',
     packages = ['googlecharts', 'googlecharts.templatetags'],
-    version = '0.1',
+    package_data = {'googlecharts': ['templates/googlecharts/*html']},
+    version = '0.1.1',
     description = 'Google Visualization API template tags and helpers for Django framework',
     author = 'Mark Vasilkov',
     author_email = 'mvasilkov@gmail.com',
