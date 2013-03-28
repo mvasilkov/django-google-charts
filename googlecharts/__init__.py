441096f76f81f27168c78183828116bbf939dfeb
Lennart Regebro <regebro@gmail.com>
2013-03-28 11:57:45 +0100

    That utility function really doesn't belong there, and adds two requirements to googlecharts that it actually never uses.

--- googlecharts/__init__.py
+++
@@ -1,5 +0,0 @@
-from qsstats import QuerySetStats
-
-def time_series(queryset, date_field, interval, func=None):
-    qsstats = QuerySetStats(queryset, date_field, func)
-    return qsstats.time_series(*interval)
